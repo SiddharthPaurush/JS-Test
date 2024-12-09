ec08889233595c709e89ef7c8f54ca8e57435887
SiddharthPaurush <116286689+SiddharthPaurush@users.noreply.github.com>
2024-12-09 14:31:19 +0000

    container config done

---
+++ 01_JS_Basics/01_Variables.js
@@ -0,0 +1,24 @@
+const accountId=14456;
+let accountMail="siddharthpaurush@gmail.com";
+var accountPassword="1234";
+accountCity="Mathura";
+let accountState;
+
+// accountId=23;//const cannot be modified
+
+accountMail="siddharthpaurush52@gmail.com";
+accountPassword="2334";
+accountCity="Delhi";
+
+// console.log(accountId);
+// console.log(accountMail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId,accountMail,accountPassword,accountCity,accountState]);
+
+
+// prefer not use var keyword for variable declaration
+// because of problems related to block scope and functional scope
+
+
